97a1df8fe2458cdd5e66e920afe988bfc885eb63
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-04-13 16:52:41 -0400

    first commit

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+
+//connect to db
+$conn = new PDO('mysql:host=sql.computerstudi.es;dbname=gc200354904', 'gc200354904', 'GBd-Wvh6');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
